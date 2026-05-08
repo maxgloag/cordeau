1d4b00041786e7dc594bf90413e08f89e081cf27
maxgloag <maxime.gloaguen@outlook.fr>
2026-05-08 15:36:42 +0200

    feat(api): configure CORS via nelmio/cors-bundle
    Autorise les origins localhost/* en dev (regex) pour le dev web
    sur http://localhost:5173. allow_credentials: true pour les cookies
    de session. La variable CORS_ALLOW_ORIGIN est injectée par env en prod.

--- apps/api/config/bundles.php
+++ apps/api/config/bundles.php
@@ -6,5 +6,6 @@ return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Nelmio\CorsBundle\NelmioCorsBundle::class => ['all' => true],
     Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
 ];
