48f21e0ec361788322affae9627a157f704cb108
maxgloag <maxime.gloaguen@outlook.fr>
2026-06-09 08:34:10 +0200

    feat(mobile): schéma SQLite photos + outbox_photos

--- apps/mobile/db/migrations/migrations.js
+++ apps/mobile/db/migrations/migrations.js
@@ -2,10 +2,12 @@
 
 import journal from "./meta/_journal.json";
 import m0000 from "./0000_flashy_wither.sql";
+import m0001 from "./0001_grey_masque.sql";
 
 export default {
   journal,
   migrations: {
     m0000,
+    m0001,
   },
 };
